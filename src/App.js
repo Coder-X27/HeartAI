1c35c8236dd29710bd58a5af307e45c0e0648bbe
Karan Sharma <sharma.karan27oct@gmail.com>
2023-12-14 22:31:33 +0530

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import './util.css';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../Assets/Logo Big.png'
+const Navbar = () => {
+  return (
+    <>
+        <div className="navbar">
+            <div className="navlogo">
+                <img src={logo} alt="" />
+            </div>
+            <div className="navlinks">
+                <a className='navlink' href="#">Telegram</a>
+                <a className='navlink' href="#">Twitter</a>
+                <a className='navlink' href="#">Docs</a>
+                <a className='navlink' href="#">$HEARTAI</a>
+            </div>
+            <button className="navbtn">Try Heart</button>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
