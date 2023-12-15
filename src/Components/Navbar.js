75828dcdb988623342de7bf22e85beb13369be05
Karan Sharma <sharma.karan27oct@gmail.com>
2023-12-15 22:45:02 +0530

    website completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import logo from '../Assets/Logo Big.png';
+import React, { useState } from "react";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import logo from '../Assets/Logo Big.png'
+
 const Navbar = () => {
-
-
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
     <>
-      <div className="navbar">
-        <div className="navlogo">
-          <img src={logo} alt="" />
+      <nav className="main-nav">
+        <div className="navlogomain">
+          <img  src={logo} alt="" />
         </div>
-        <div className="navlinks">
-          <a className='navlink' href="/">Telegram</a>
-          <a className='navlink' href="/">Twitter</a>
-          <a className='navlink' href="/">Docs</a>
-          <a className='navlink' href="/">$HEARTAI</a>
+
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+          <ul>
+            <a href="/"><li>
+            Telegram
+            </li></a>
+            <a href="/"><li>
+              Twitter
+            </li></a>
+            <a href="/"><li>
+              Docs
+            </li></a>
+            <a href="/"><li>
+              $MND
+            </li></a>
+            <li className="trymini">
+              Try Mini
+            </li>
+          </ul>
         </div>
-        <button className="navbtn">Try Heart</button>
-      </div>
+        <div className="social-media">
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
